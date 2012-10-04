c535eb943835dcc8bbe801aca19491209d0e3ce0
Vincent Bonamy <vbonamy@gmail.com>
2012-10-04 14:35:14 +0200

    EMAILPLT-79: Add support for proxy CAS

---
+++ src/main/java/org/jasig/portlet/emailpreview/service/auth/CasAuthenticationService.java
@@ -0,0 +1,105 @@
+/**
+ * Licensed to Jasig under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Jasig licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jasig.portlet.emailpreview.service.auth;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import javax.mail.Authenticator;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletSession;
+
+import org.jasig.cas.client.validation.Assertion;
+import org.jasig.portlet.cas.ICASProxyTicketService;
+import org.jasig.portlet.emailpreview.MailStoreConfiguration;
+import org.jasig.portlet.emailpreview.service.ConfigurationParameter;
+
+public class CasAuthenticationService implements IAuthenticationService {
+    
+    public static final String KEY = "cas";
+    
+    protected ICASProxyTicketService casTicketService;
+    protected String serviceUrl;  
+    
+    protected String CAS_ASSERTION_KEY = "CAS_ASSERTION_KEY";
+    
+    public void setCasTicketService(ICASProxyTicketService casTicketService) {
+		this.casTicketService = casTicketService;
+	}
+
+	public void setServiceUrl(String serviceUrl) {
+		this.serviceUrl = serviceUrl;
+	}
+	
+	protected String getProxyPrincipalname(PortletRequest request) {
+		Assertion casAssertion = getCasAssertion(request);
+		String proxyPrincipalname = casAssertion.getPrincipal().getName();
+		return proxyPrincipalname;
+	}
+		
+	protected Assertion getCasAssertion(PortletRequest request) {
+		PortletSession session = request.getPortletSession(false);
+	    Assertion casAssertion = (Assertion) session.getAttribute(CAS_ASSERTION_KEY);
+	    if(casAssertion == null) {
+	    	casAssertion = casTicketService.getProxyTicket(request);
+			if(casAssertion == null)
+				throw new RuntimeException("CasAssertion is null : Auth ProxyCAS failed ??");
+		    session.setAttribute(CAS_ASSERTION_KEY, casAssertion);     
+	    }
+	    return casAssertion;
+	}
+
+
+	
+    @Override
+    public Map<String, ConfigurationParameter> getConfigurationParametersMap() {
+        return Collections.<String, ConfigurationParameter>emptyMap();
+    }
+
+    @Override
+    public boolean isConfigured(PortletRequest request, MailStoreConfiguration config) {
+        return true;
+    }
+
+    public Authenticator getAuthenticator(PortletRequest request, MailStoreConfiguration config) {
+    	Assertion casAssertion = getCasAssertion(request);
+    	String proxyPrincipalname = getProxyPrincipalname(request);
+    	String proxyTicket = casTicketService.getCasServiceToken(casAssertion, serviceUrl);    
+        return new SimplePasswordAuthenticator(proxyPrincipalname, proxyTicket);
+    }
+    
+    public String getMailAccountName(PortletRequest request, MailStoreConfiguration config) {
+    	String proxyPrincipalname = getProxyPrincipalname(request);
+        return proxyPrincipalname;
+    }
+
+    public String getKey() {
+        return KEY;
+    }
+
+    public List<ConfigurationParameter> getAdminConfigurationParameters() {
+        return Collections.<ConfigurationParameter>emptyList();
+    }
+
+    public List<ConfigurationParameter> getUserConfigurationParameters() {
+        return Collections.<ConfigurationParameter>emptyList();
+    }
+
+}
