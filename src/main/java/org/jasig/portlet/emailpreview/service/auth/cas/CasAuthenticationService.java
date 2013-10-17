538df92da021d3aadd7242664a3985bc4e9089c7
James Wennmacher <jwennmacher@unicon.net>
2013-10-17 11:50:08 -0700

    EMAILPLT-157 Fix CAS integration
    Alternate implementation to https://github.com/Jasig/email-preview/pull/52/files

--- src/main/java/org/jasig/portlet/emailpreview/service/auth/cas/CasAuthenticationService.java
+++ src/main/java/org/jasig/portlet/emailpreview/service/auth/cas/CasAuthenticationService.java
@@ -19,16 +19,21 @@
 package org.jasig.portlet.emailpreview.service.auth.cas;
 
 import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.jasig.cas.client.validation.Assertion;
 import org.jasig.portlet.emailpreview.MailStoreConfiguration;
 import org.jasig.portlet.emailpreview.service.auth.BaseCredentialsAuthenticationService;
 import org.jasig.portlet.emailpreview.service.auth.SimplePasswordAuthenticator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.mail.Authenticator;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletSession;
 
 public class CasAuthenticationService extends BaseCredentialsAuthenticationService {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
     
     protected String key = "cas";
     
@@ -75,8 +80,18 @@ public class CasAuthenticationService extends BaseCredentialsAuthenticationServi
         return new SimplePasswordAuthenticator(proxyPrincipalname, proxyTicket);
     }
 
-    public Credentials getCredentials(PortletRequest req, MailStoreConfiguration config) {
-        throw new UnsupportedOperationException("CAS ticket doesn't make sense with Exchange integration");
+    /**
+     * CAS ticket doesn't make sense with Exchange Integration.  However this method is invoked as part of the
+     * services layer initialization and is used to obtain the username so create a credential with just the username.
+     * We also don't want to create a proxy ticket because that would be a 2nd trip to the CAS server (or we'd need to
+     * obtain the proxy ticket and cache it to the thread so the initialization code can call both getAuthenticator
+     * and getCredentials).
+     */
+    public Credentials getCredentials(PortletRequest request, MailStoreConfiguration config) {
+        String proxyPrincipalname = getProxyPrincipalname(request);
+        log.debug("CAS ticket doesn't make sense with Exchange integration, creating null password credential for "
+                + proxyPrincipalname);
+        return new UsernamePasswordCredentials(proxyPrincipalname, null);
     }
 
     public String getMailAccountName(PortletRequest request, MailStoreConfiguration config) {
